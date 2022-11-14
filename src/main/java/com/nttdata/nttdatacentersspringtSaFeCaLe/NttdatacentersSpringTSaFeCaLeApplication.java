fe1d9bb91cd1a19713b78b816f3917601ce24b35
Salva Castillo <salvadorfcl85@gmail.com>
2022-11-14 23:11:06 +0100

    Creación del proyecto

---
+++ src/main/java/com/nttdata/nttdatacentersspringtSaFeCaLe/NttdatacentersSpringTSaFeCaLeApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.nttdatacentersspringtSaFeCaLe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NttdatacentersSpringTSaFeCaLeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NttdatacentersSpringTSaFeCaLeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/nttdatacentersspringtSaFeCaLe/NttdatacentersSpringTSaFeCaLeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.nttdatacentersspringtSaFeCaLe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NttdatacentersSpringTSaFeCaLeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
